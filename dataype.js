beb8b46026b95acc440586afcf5836b1853c05d7
abiral-adhikari <abiraladhikari1222@gmail.com>
2024-10-28 19:47:25 +0545

    Data Types

---
+++ dataype.js
@@ -0,0 +1,34 @@
+console.log(typeof(2))
+console.log(Math.random())
+console.log(Math.PI)
+
+// ParseInt(str,base-n)
+console.log(parseInt("1000",2)+22)
+
+// convert string to number with + 
+console.log(typeof(+"12"))
+console.log(isNaN("hello"))
+
+// String
+console.log("My Name is Abiral".length)
+console.log("Abiral".charAt(1))
+console.log("abiral adhikari".toUpperCase())
+
+// Boolean
+var shouldBeTrue= true
+var shouldBeNull =null
+console.log(shouldBeTrue===true)
+console.log(shouldBeNull)
+
+var Content="kjladf"
+console.log(Boolean(Content)==true)
+console.log(Content===true)
+
+var some
+console.log(typeof(some))
+console.log(Boolean(some))
+console.log(Boolean(NaN))
+
+// if null == undefined true but === is false
+console.log(null==undefined)
+console.log(null===undefined)
